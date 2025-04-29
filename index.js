9b6bb36ea28490938f9163a4f16f0cf1f1af9501
banyaigabor <banyai.gabor@promir.hu>
2025-04-29 14:40:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -674,7 +674,7 @@ app.post('/form/submit', async (req, res) => {
               'Beírt útidő (ó)': parseFloat(submittedData.Distance_Time_SL),
               'Kalkulált útidő (ó)': parseFloat(submittedData.Distance_SL) / 70, // kalkuláció példa: 70 km/h sebességgel
              // 'Szerepkör': submittedData.radio_button,
-              'Kiszállás Dátuma': submittedData.date,
+              //'Kiszállás Dátuma': submittedData.date,
              // 'Rendszám': submittedData.PlateNumber_dropdown
             }
           });
